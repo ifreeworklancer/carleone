f41c8ed8387ca967082467d3c4a4d4115e300661
ifreeworklancer <ifreeworklancer@gmail.com>
2018-11-19 09:40:21 +0200

    #home

--- views/sections/advantages.php
+++ views/sections/advantages.php
@@ -1,4 +1,25 @@
 <!-- advantages -->
 <section id="advantages">
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-lg-6">
+                
+            </div>
+            <div class="col-sm-8 col-lg-6">
+                <?php 
+                    $count = 0;
+                    foreach ($advantages['list'] as $item) {
+                        $count++;
+                        echo '<div class="advantages-item">'
+                        .
+                        '<div class="advantages-item__count"><span>' .  $count . '</span></div>'
+                        .
+                        '<div class="advantages-item__description">' .  $item['item'] . '</div>'
+                        .
+                        '</div>';
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
 </section>
--- views/sections/intro.php
+++ views/sections/intro.php
@@ -1,19 +1,47 @@
 <!-- Intro -->
 <section id="intro">
     <div class="parallax">
-        <div class="parallax-item parallax-item--1"></div>
-        <div class="parallax-item parallax-item--2"></div>
+        <div class="parallax__item parallax__item--1" data-speed="1"></div>
+        <div class="parallax__item parallax__item--2" data-speed="20"></div>
+        <div class="parallax__item parallax__item--3" data-speed="40"></div>
+        <div class="no-parallax"></div>
     </div>
     <div class="container h-100">
-        <div class="row h-100">
-            <div class="col-sm-9 col-lg-7 col-xl-6 h-100 d-flex align-items-center">
+        <div class="row h-100 justify-content-between">
+            <div class="col-sm-7 col-xl-6 h-100 d-flex align-items-center mb-4 mb-sm-0">
                 <div class="intro-item">
                     <h1 class="intro-item__title">
-                        <?= $intro['title'] ?>    
+                        <?= $intro['title'] ?>
                     </h1>
                     <div class="intro-item__subtitle">
                         <?= $intro['subtitle'] ?>
                     </div>
+                </div>
+            </div>
+            <div class="col-sm-5 col-xl-4 d-flex align-items-end">
+                <div class="intro-order">
+                    <h3 class="intro-order__title text-primary text-uppercase">
+                        Оставить заявку
+                    </h3>
+                    <div class="intro-order__description text-white mb-4">
+                        Хотите убедиться, что получится выгоднее?
+                        Оставьте заявку, мы рассчитаем стоимость
+                    </div>
+                    <form>
+                        <div class="form-column">
+                            <div class="form-group">
+                                <label for="user-name--intro" class="label-placeholder">Введите ваше имя</label>
+                                <input type="text" name="name" class="form-control" id="user-name--intro">
+                            </div>
+                            <div class="form-group">
+                                <label for="user-phone--intro" class="label-placeholder">Номер телефона</label>
+                                <input type="phone" name="phone" class="form-control" id="user-phone--intro">
+                            </div>
+                            <div class="form-group form-groupc--btn">
+                                <button type="submit" class="btn btn-primary">Оставить заявку</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
